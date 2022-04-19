c44c640ef5ca16e6ad6e6064fcaf07af96d7c725
João Batista da Rocha Neto <framework25@localiza.com>
2022-04-18 23:43:20 -0300

    Criação das camadas da solução; Configuração do Swagger

---
+++ src/LI.Carrinho.API/Controllers/CarrinhoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LI.Carrinho.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CarrinhoController : ControllerBase
+    {
+        /// <summary>
+        /// Saúde da aplicação
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult HealthCheck() => Ok("A aplicação está saudável!");
+    }
+}
--- src/LI.Carrinho.API/Startup.cs
+++ src/LI.Carrinho.API/Startup.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace LI.Carrinho.API
 {
@@ -23,7 +21,29 @@ namespace LI.Carrinho.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting(options => options.LowercaseUrls = true);
+
+            services.AddControllers();
+
+            services.AddHealthChecks();
+
             services.AddRazorPages();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "LI.Carrinho.API",
+                    Description = "Desafio Loja Integrada",
+                    Version = "v1"
+                });
+
+                var apiPath = Path.Combine(AppContext.BaseDirectory, "LI.Carrinho.API.xml");
+                var applicationPath = Path.Combine(AppContext.BaseDirectory, "LI.Carrinho.Application.xml");
+
+                c.IncludeXmlComments(apiPath);
+                c.IncludeXmlComments(applicationPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,15 +53,15 @@ namespace LI.Carrinho.API
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
+
+            app.UsePathBase("/lojaIntegrada-carrinho-api-netcore");
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
             {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+                c.SwaggerEndpoint("/lojaIntegrada-carrinho-api-netcore/swagger/v1/swagger.json", "API LI.Carrinho.API");
+            });
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -49,7 +69,8 @@ namespace LI.Carrinho.API
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health");
             });
         }
     }
